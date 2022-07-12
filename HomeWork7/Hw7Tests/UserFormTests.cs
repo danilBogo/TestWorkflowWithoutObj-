deb64b210bd209b3e102e8ccb3cb7de4bd9c6e3e
MaratElagin <maratelagin@gmail.com>
2022-07-12 14:59:16 +0300

    changes

--- HomeWork7/Hw7Tests/UserFormTests.cs
+++ HomeWork7/Hw7Tests/UserFormTests.cs
@@ -43,7 +43,7 @@ public class UserFormTests : IClassFixture<WebApplicationFactory<Program>>
         //act
         var actual = TestHelper.TryGetSelect(response, "Sex");
 
-        //assert
+        //assertцуцуцуц
         Assert.True(actual);
     }
     
